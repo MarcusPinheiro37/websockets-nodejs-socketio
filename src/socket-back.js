c76e7b7b40b2712edd0c0788cd66dd9c1e3ab6e0
Marcola <marcusmagno34@gmail.com>
2024-04-13 21:08:19 -0300

    Adicionado as interações com o banco de dados

--- src/socket-back.js
+++ src/socket-back.js
@@ -18,6 +18,25 @@ const io = new Server(servidorHttp, {
 export default io.on('connection', (socket) => {
     console.log('Um cliente se conectou. ID:', socket.id);
 
+    socket.on('obter_documentos', async (devolverDocumentos) => {
+        const documentos = await DocumentoController.listaDocumentos();
+        devolverDocumentos(documentos);
+    });
+
+    socket.on('adicionar_documento', async (nomeDocumento) =>{
+        const documentoExiste = (await DocumentoController.listaUmDocumento(nomeDocumento)) !== null;
+        
+        if(documentoExiste){
+            socket.emit('documento_existente', nomeDocumento);
+        } else{
+            const resultado = await DocumentoController.adicionaDocumentos(nomeDocumento);
+            console.log(resultado);
+            if(resultado.insertedCount){
+                io.emit('adicionar_documento_interface', nomeDocumento);
+            }
+        }
+    });
+
     socket.on('selecionar_documento', async (nomeDocumento, devolverTexto) => {
         
         socket.join(nomeDocumento);
@@ -25,6 +44,13 @@ export default io.on('connection', (socket) => {
 
         if(documento) {
             devolverTexto(documento.texto);
+        }
+    });
+
+    socket.on('deleta_documento', async (nomeDocumento) => {
+        const deletaDocumento = await DocumentoController.deletaDocumento(nomeDocumento);
+        if(deletaDocumento.deletedCount){
+            io.emit('excluir_documento_sucesso', nomeDocumento);
         }
     });
 
